b79c9dab99bcc99819195004582334b209c3e0ab
vulbecula <vulbecula@gmail.com>
2018-01-12 14:31:12 +0200

    Exeption handling and spending account logic

--- spammer.py
+++ spammer.py
@@ -5,12 +5,16 @@ import datetime
 from base64 import b64encode
 
 
-#Wallet allowed rpc address
+#Wallet allowed rpc address and port
 CONN = http.client.HTTPConnection("127.0.0.1:35001")
 #RPC user
-RPCUSER="x"
+RPCUSER = "x"
 #RPC password
-RPCPASSWORD="y"
+RPCPASSWORD = "y"
+#timeinterval for sends
+TIMEOUT_BETWEEN_SENDS = 3.0 #value in seconds
+#send amount
+SEND_AMOUNT = 1.00
 
 AUTH = str(RPCUSER+":"+RPCPASSWORD)
 HEADERS = {
@@ -23,9 +27,9 @@ HEADERS = {
 #defaults to getbalance if no paramaters
 def action(rpcaction = "getbalance", params=None):
     
-    if params == None: 
+    if params is None: 
         payload = {"jsonrpc": 2.0, "id": 0, "method": rpcaction}
-    elif params != None:
+    elif params is not None:
         payload = {"jsonrpc": 2.0, "id": 0, "method": rpcaction, "params": params}
     CONN.request("POST", "", json.dumps(payload), HEADERS)
     res = CONN.getresponse()
@@ -42,25 +46,42 @@ def getaddresses():
 def main():
     #get addresses where to send
     addresses = getaddresses()
-    #get current wallet balance
-    balance = json.loads(action("getbalance"))["result"]
-    #inits
+    #get wallet accounts
+    accountnames = json.loads(action("listaccounts"))["result"]
 
     #loop until wallet balance over 10xby
-    while(balance > 10):
-        sendaccounts = {}
-        counter = 0
-        #go over addresses and add to payee list there is balance left
-        for address in addresses:
-            #check  if wallet has enough for transaction fee, if not don't add address to payee list
-            if balance >= 2:            
-                sendaccounts.update({address.rstrip():1.00})
-                balance = balance - 2
-                counter =  counter + 1
-        #make send many call to wallet
-        res = action("sendmany", ["", sendaccounts])
-        print(res)
-        print("sended to {} accounts, remaining balance: {}".format(counter, balance))
-        time.sleep(30)
-        
+    while(True):
+        try:
+            #send by wallet account
+            for account in accountnames.keys():
+                if account is None:
+                    account = "\"\""
+                sendaccounts = {}
+                counter = 0
+                #get current account balance        
+                balance = json.loads(action("getbalance", [account]))["result"]
+                if balance > SEND_AMOUNT:
+                    print("//////////////////////////////////////")
+                    print("Sendig from account: \"{}\" balance: {}".format(account, balance))
+                    #go over addresses and add to payee list if there is balance left
+                    for address in addresses:
+                        #check  if wallet has enough for transaction fee, if not, don't add address to payee list
+                        if balance >= SEND_AMOUNT:            
+                            sendaccounts.update({address.rstrip():SEND_AMOUNT})
+                            balance = balance - 2.0 # I don't know whats real transaction fee per address in sendmamy, so using 1
+                            counter =  counter + 1
+                    #make sendmany call to wallet
+                    res = action("sendmany", [account, sendaccounts])
+                    resobj = json.loads(res)
+                    if(resobj["result"] is None and resobj["error"] is not None):
+                        print("Error occurred, response: {}".format(res))
+                    else:
+                        print("Send done, transaction id: {}".format(resobj["result"]))
+                    print("sended to {} accounts, from Account \"{}\" calculated \"remaining\" balance: {}".format(counter, account, balance))
+                    print("//////////////////////////////////////")
+                    print("press Ctrl+C to stop")
+                    time.sleep(TIMEOUT_BETWEEN_SENDS)
+        except KeyboardInterrupt:
+            break
+
 if __name__ == "__main__": main()
